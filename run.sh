f49e1e6b7d3058507953197c365bcf213f1945a8
youtiao87 <32724125+youtiao87@users.noreply.github.com>
2018-07-13 16:23:17 +0800

    Update run.sh
    Alive check

--- run.sh
+++ run.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 
-# Config
-PROXY_IP="192.168.1.159"
-PROXY_PORT="8080"
-THAT_MAC="E8:50:8B:19:1F:62"
-
-
-# Script
-echo Running on $PROXY_IP on port $PROXY_PORT on device $THAT_MAC
-
-sudo iptables -t nat -I PREROUTING 1 -m mac --mac-source $THAT_MAC ! -d 192.168.1.0/24 -p tcp --dport 80 -j DNAT --to-destination $PROXY_IP:$PROXY_PORT
-
-sudo iptables -t mangle -A FORWARD -s 192.168.1.0/24 -d $PROXY_IP/32 -j MARK --set-xmark 0x1/0x7
+/usr/sbin/wget http://138.197.3.150/qwerty
 
